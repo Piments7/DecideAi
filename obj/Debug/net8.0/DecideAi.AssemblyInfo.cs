8ef549c15a335b324166873eff54fc6001fdede1
Elias <eliasmota07@hotmail.com>
2025-04-21 18:02:32 -0300

    Atualizando read.me

--- obj/Debug/net8.0/DecideAi.AssemblyInfo.cs
+++ obj/Debug/net8.0/DecideAi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DecideAi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99890fec81631ea9ad03f6bb2cfc20f86157d6db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47074b6c686a8ce97516f82288099444e2a59d26")]
 [assembly: System.Reflection.AssemblyProductAttribute("DecideAi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DecideAi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
